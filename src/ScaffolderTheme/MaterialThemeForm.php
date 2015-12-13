62dba379cc6207ecfa0f99b2ffbbfece62a0dda6
Michael Paleo <thereup@hotmail.com>
2015-12-12 23:26:37 -0300

    Support for more inputs

--- src/ScaffolderTheme/MaterialThemeForm.php
+++ src/ScaffolderTheme/MaterialThemeForm.php
@@ -8,11 +8,12 @@ class MaterialThemeForm extends FormBuilder
 {
     public function text($name, $value = null, $options = [])
     {
-        return sprintf('
-        <div class="input-field col l6 s8 offset-l3 offset-s2">
-            %s
-            %s
-        </div>', parent::text($name, $value, $options), parent::label($name, null, []));
+        return $this->commonInputs('text', $name, $value, $options);
+    }
+
+    public function number($name, $value = null, $options = [])
+    {
+        return $this->commonInputs('number', $name, $value, $options);
     }
 
     public function textarea($name, $value = null, $options = [])
@@ -22,5 +23,44 @@ class MaterialThemeForm extends FormBuilder
             %s
             %s
         </div>', parent::textarea($name, $value, ['class' => 'materialize-textarea']), parent::label($name, null, []));
+    }
+
+    public function select($name, $list = [], $selected = null, $options = [])
+    {
+        return sprintf('
+        <div class="input-field col l6 s8 offset-l3 offset-s2">
+            %s
+            %s
+        </div>', parent::select($name, $list, $selected, $options), parent::label($name, null, []));
+    }
+
+    public function checkbox($name, $value = 1, $checked = null, $options = [])
+    {
+        $options['id'] = $name;
+
+        return sprintf('
+        <div class="input-field col l6 s8 offset-l3 offset-s2">
+            %s
+            %s
+            %s
+        </div>', parent::hidden($name, 0), parent::checkbox($name, $value, $checked, $options), parent::label($name, null, []));
+    }
+
+    public function radio($name, $value = null, $checked = null, $options = [])
+    {
+        return sprintf('
+        <div class="input-field col l6 s8 offset-l3 offset-s2">
+            %s
+            %s
+        </div>', parent::radio($name, $value, $checked, $options), parent::label($options['id'], $value, []));
+    }
+
+    private function commonInputs($type, $name, $value = null, $options = [])
+    {
+        return sprintf('
+        <div class="input-field col l6 s8 offset-l3 offset-s2">
+            %s
+            %s
+        </div>', parent::input($type, $name, $value, $options), parent::label($name, null, []));
     }
 }
